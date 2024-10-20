3a3576a762a713fd372c589b193858221468a785
Lucas Cintra <lucasrcintra63@gmail.com>
2024-10-20 17:41:22 -0300

    exercicio 5

--- Exercício/exercicio5.py
+++ Exercício/exercicio5.py
@@ -1,14 +1,54 @@
 class Filial:
-  def __init__(self, nome, cidade):
-    self.nome = nome
-    self.cidade = cidade
+  def __init__(self):
+    self.diretor = None
+
+  def get_diretor(self):
+    return self.diretor
+
+  def set_diretor(self, nome):
+    self.diretor = nome
+  
+  def get_nome_diretor_empresa(self):
+    if self.diretor == None:
+      print("Diretor não informado")
+      exit()
+    else:
+      return self.diretor.get_nome_funcionario()
 
 class Empresa:
-  def __init__(self, nome, filial):
-    self.nome = nome
-    self.filial = filial
+  def __init__(self, funcionario):
+    self.set_funcionario(funcionario)
+  
+  def get_funcionario(self):
+    return self.funcionario
+
+  def set_funcionario(self, funcionario):
+    if funcionario == None:
+      print("Funcionario não informado")
+      exit()
+    else:
+      self.funcionario = funcionario
+  
+  def get_nome_funcionario(self):
+    return self.funcionario.get_nome()
+    
 
 class Funcionario:
-  def __init__(self, nome, filial):
+  def __init__(self):
+    self.nome = ""
+
+  def get_nome(self):
+    return self.nome
+
+  def set_nome(self, nome):
     self.nome = nome
-    self.filial = filial
+  
+funcionario = Funcionario()
+funcionario.set_nome("Lucas")
+
+empresa = Empresa(funcionario)
+
+filial = Filial()
+filial.set_diretor(empresa)
+
+print(filial.get_nome_diretor_empresa())
