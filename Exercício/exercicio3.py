63aa98b78d6929604fdda4f457149fd8bbad8147
Lucas Cintra <lucasrcintra63@gmail.com>
2024-10-20 17:24:12 -0300

    exercicio 3

--- Exercício/exercicio3.py
+++ Exercício/exercicio3.py
@@ -1,18 +1,67 @@
-class Estado:
-  def __init__(self, nome):
-    self.nome = nome
+class Funcionario:
+  def __init__(self):
+    self.estado = None
 
-class Cidade:
-  def __init__(self, nome, estado):
-    self.nome = nome
+  def get_estado(self):
+    return self.estado
+  
+  def set_estado(self, estado):
     self.estado = estado
 
-class Filial:
-  def __init__(self, nome, cidade):
-    self.nome = nome
-    self.cidade = cidade
+  def get_nome_estado_filial(self):
+    return self.estado.get_nome_estado_cidade()
 
-class Funcionario:
-  def __init__(self, nome, filial):
-    self.nome = nome
-    self.filial = filial
+class Filial:
+  def __init__(self):
+    self.cidade = None
+
+  def get_cidade(self):
+    return self.cidade
+  
+  def set_cidade(self, estado):
+    self.cidade = estado
+  
+  def get_nome_estado_cidade(self):
+    return self.cidade.get_nome_estado()
+
+
+class Cidade:
+  def __init__(self, estado):
+    self.set_estado(estado)
+
+  def get_estado(self):
+    return self.estado
+  
+  def set_estado(self, estado):
+        if estado == None:
+            print("Cidade não pode ficar sem estado")
+            exit()
+        self.estado = estado
+
+  def get_nome_estado(self):
+    return self.estado.get_nome()
+    
+
+class Estado:
+  def __init__(self):
+    self.nome = None
+
+  def get_nome(self):
+    return self.nome
+  
+  def set_nome(self, estado):
+    self.nome = estado
+
+
+estado = Estado()
+estado.set_nome("Minas Gerais")
+
+cidade = Cidade(estado)
+
+filial = Filial()
+filial.set_cidade(cidade)
+
+funcionario = Funcionario()
+funcionario.set_estado(filial)
+
+print(funcionario.get_nome_estado_filial())
